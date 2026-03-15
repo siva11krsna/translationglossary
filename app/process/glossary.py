ef9dcf110471819b55788f31730578eb1d4bf3fc
sivakumar <siva11krsna@gmail.com>
2026-03-15 13:14:34 +0530

    initial check-in

---
+++ app/process/glossary.py
@@ -0,0 +1,8 @@
+from nltk import *
+
+def getGlossary():
+
+    translation = "Dhṛtarāṣṭra said: O Sañjaya, after my sons and the sons of Pāṇḍu assembled in the place of pilgrimage at Kurukṣetra, desiring to fight, what did they do?"
+
+    print(word_tokenize(translation))
+getGlossary()
